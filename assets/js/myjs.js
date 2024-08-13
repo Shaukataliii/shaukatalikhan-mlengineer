d536748b18267f25c6c02ba499263f0cf68bcc72
Shaukataliii <youngdevelopersk@gmail.com>
2024-08-13 08:18:12 +0500

    Updated projects on home page, favicon, page title, optimized images, removed navigation menu and updated gitignore file

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -1,15 +1,15 @@
-// Get the container element
+// Get the projects container
 const projectsContainer = document.getElementById('projects-page-projects');
 		
 // Check if the container is found
 if (projectsContainer) {
-    // Get all child elements with class 'spotlight'
+    // Get all child elements with class 'project'
     const projects = projectsContainer.querySelectorAll('.project');
 
-    // Count the number of items
+    // Count the number of projects
     const projectsCount = projects.length;
 
-    // Update background color for each item
+    // Update background color for each item (increasing opacticy with constant step size)
     projects.forEach((project, index) => {
         project.style.backgroundColor = getAlphaAdjustedColor(index);
     });
